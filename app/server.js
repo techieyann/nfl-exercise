7cacab6c7edde4d9a826cfdbc558fdbf20c937a5
ian <techieyann@gmail.com>
2013-09-06 13:11:25 -0700

    hello world, thanks howtonode.org!

--- app/server.js
+++ app/server.js
@@ -1,9 +1,17 @@
 /**
- *
- *
- *
- *    [Your App Here]
- *
- *
- *
+file: app/server.js
+creator: Ian McEachern
+
+This is a node app for displaying and filtering sample NFL data
  */
+
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("hello world!");
+});
+
+server.listen(8080);
+
+console.log("Server running on port 8080");
